9085a6fcc93658838c8ca37f5d45c2e2d83ab41a
7007singh <shw12021982@gmail.com>
2023-01-23 20:55:34 +0530

    [Shweta]Added: Length calculation using cartesian system

---
+++ LineComparisonProblem/CalculateLength.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LineComparisonProblem
+{
+    internal class CalculateLength
+    {
+        public double x1, x2, y1, y2, distance;
+        public CalculateLength(double a, double b, double c, double d)
+        {
+            x1 = a;
+            x2 = b;
+            y1 = c;
+            y2 = d;
+        }
+        public void Calculate()
+        {
+            distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("distance of line: " + distance);
+        }
+    }
+}
--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,22 @@ namespace LineComparisonProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Program");
-            Console.ReadLine();
+
+            Console.WriteLine("Enter the value of x2");
+            double val1 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Enter the value of x1");
+            double val2 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Enter the value of y2");
+            double val3 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Enter the value of y1");
+            double val4 = Convert.ToDouble(Console.ReadLine());
+
+            CalculateLength length = new CalculateLength(val1, val2, val3, val4);
+            length.Calculate();
+            Console.Read();
         }
     }
 }
